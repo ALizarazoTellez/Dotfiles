8912fe6ab7bc53e6029c255d16634ff594fa061c
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-09-18 22:51:38 -0500

    Add plugin Pure to Fish
    This is a custom prompt.

---
+++ Configuration/fish/conf.d/pure.fish
@@ -0,0 +1,7 @@
+# Configuration of `Pure.fish`
+
+# The official page recommends using universal variables, but here I use
+# global variables to manage dotfiles more easily.
+
+set --global pure_show_jobs true
+set --global pure_enable_nixdevshell true
