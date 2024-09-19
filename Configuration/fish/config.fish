cc2c0dfa4331b528f1e63d0c8175b71f3666becc
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-09-18 21:44:46 -0500

    Use zoxide instead of built-in `cd` in Fish

--- Configuration/fish/config.fish
+++ Configuration/fish/config.fish
@@ -1,6 +1,9 @@
 # Copyright (C) 2024, Anderson Lizarazo Tellez.
 
 if status is-interactive
+    # A smarter `cd` command.
+    zoxide init fish --cmd cd | source
+
     abbr --add ls -- eza
     abbr --add tree -- eza -T
     abbr --add vim -- nvim
