1f2abf9519c812930c5c5d288b2c48f4c5e13de2
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-10-08 17:05:36 -0500

    Ensure that `~/.local/bin` has priority over other paths in `$PATH`

--- Configuration/fish/conf.d/environment.fish
+++ Configuration/fish/conf.d/environment.fish
@@ -1,10 +1,10 @@
 # User environment variables.
 
+set PATH $PATH ~/.local/bin
+
 # Go.
 set --export GOPATH ~/.local/share/go
 set PATH $PATH $GOPATH/bin
 
 # Rust.
 set PATH $PATH ~/.cargo/bin
-
-set PATH $PATH ~/.local/bin
