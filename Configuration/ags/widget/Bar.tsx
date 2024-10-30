7767b6d7373f23f69ca5982121de36e2ae6bf288
Anderson Lizarazo Tellez <andersonlizarazo27@gmail.com>
2024-10-30 01:21:11 -0500

    Add a basic AGS status bar

---
+++ Configuration/ags/widget/Bar.tsx
@@ -0,0 +1,111 @@
+import { App, Variable, Astal, Gtk, Gdk, GLib, bind } from "astal";
+import Tray from "gi://AstalTray";
+import Hyprland from "gi://AstalHyprland";
+import Battery from "gi://AstalBattery";
+
+export default function Bar(monitor: number) {
+  return (
+    <window
+      name="Bar"
+      application={App}
+      className="Bar"
+      monitor={monitor}
+      anchor={
+        Astal.WindowAnchor.TOP |
+        Astal.WindowAnchor.LEFT |
+        Astal.WindowAnchor.RIGHT
+      }
+    >
+      <centerbox>
+        <Workspaces />
+        <Time />
+        <box halign={Gtk.Align.END}>
+          <SysTray />
+          <BatteryLevel />
+        </box>
+      </centerbox>
+    </window>
+  );
+}
+
+function Workspaces() {
+  const hypr = Hyprland.get_default();
+
+  return (
+    <box className="Workspaces">
+      {bind(hypr, "workspaces").as((wss) =>
+        wss
+          .sort((a, b) => a.id - b.id)
+          .filter((ws) => ws.id > 0)
+          .map((ws) => (
+            <button
+              className={bind(hypr, "focusedWorkspace").as((fw) =>
+                ws === fw ? "focused" : "",
+              )}
+              onClicked={() => ws.focus()}
+            >
+              {ws.id}
+            </button>
+          )),
+      )}
+    </box>
+  );
+}
+
+function Time({ format = "%H:%M" }) {
+  const time = Variable<string>("").poll(
+    1000,
+    () => GLib.DateTime.new_now_local().format(format)!,
+  );
+
+  return (
+    <label className="Time" onDestroy={() => time.drop()} label={time()} />
+  );
+}
+
+function SysTray() {
+  const tray = Tray.get_default();
+
+  return (
+    <box>
+      {bind(tray, "items").as((items) =>
+        items.map((item) => {
+          if (item.iconThemePath) App.add_icons(item.iconThemePath);
+
+          const menu = item.create_menu();
+
+          return (
+            <button
+              tooltipMarkup={bind(item, "tooltipMarkup")}
+              onDestroy={() => menu?.destroy()}
+              className={"SysTray-item"}
+              onClickRelease={(self) => {
+                menu?.popup_at_widget(
+                  self,
+                  Gdk.Gravity.SOUTH,
+                  Gdk.Gravity.NORTH,
+                  null,
+                );
+              }}
+            >
+              <icon gIcon={bind(item, "gicon")} />
+            </button>
+          );
+        }),
+      )}
+    </box>
+  );
+}
+
+function BatteryLevel() {
+  const bat = Battery.get_default();
+
+  return (
+    <box className="Battery" visible={bind(bat, "isPresent")}>
+      <icon icon={bind(bat, "batteryIconName")} />
+      <label
+        label={bind(bat, "percentage").as((p) => ` ${Math.floor(p * 100)}%`)}
+      />
+    </box>
+  );
+}
